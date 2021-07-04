b67a3bcaca9591cfbb1781ae9b76c2a472b1ab6c
SaraAlthubaiti <sara.althubaiti@kaust.edu.sa>
2021-07-04 16:19:59 +0300

    Create annotate_vcf.sh

---
+++ step-by-step/annotate_vcf.sh
@@ -0,0 +1,28 @@
+#!/bin/bash 
+
+#SBATCH --mem 80Gb # memory pool for all cores
+#SBATCH --time 24:00:00 # time, specify max time allocation
+#SBATCH --gres=gpu
+#SBATCH --mail-type=END,FAIL # notifications for job done & fail
+#SBATCH --mail-user=sara.althubaiti@kaust.edu.sa
+#SBATCH --job-name=Annotategene
+#SBATCH --output=/encrypted/e3008/Sara/HNSC.log
+
+# the path to input/samples folder
+path_to_vcf='/encrypted/e3008/Sara/cancer_types/TCGA-HNSC/vcf'
+mkdir /encrypted/e3008/Sara/cancer_types/TCGA-HNSC/vcf/output
+# the path to Results folder
+path_to_output='/encrypted/e3008/Sara/cancer_types/TCGA-HNSC/vcf/output'
+
+# the path to annovar folder
+path_to_annovar='/encrypted/e3000/gatkwork/annovar'
+
+for entry in $path_to_vcf/*.vcf.gz
+do
+
+fbname=$(basename "$entry" .vcf.gz)
+
+# start annotate the vcf file
+perl "$path_to_annovar"/table_annovar.pl "$path_to_vcf/$fbname".vcf.gz humandb/ -buildver hg38 -out "$path_to_output/OutputAnnoFile_$fbname" -remove -protocol ensGene,avsnp147,dbnsfp30a -operation g,f,f -nastring . -vcfinput
+
+done
